714f2e061870e1ca6d65db7b24fb225c3b11c18c
Muhammad Ilmi Alimudin <muhamad.ilmi86@gmail.com>
2025-03-05 14:19:48 +0700

    upd sh

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,19 +1,19 @@
-# !/bin/bash
+#!/bin/bash
 
 # Hentikan eksekusi jika terjadi error
 set -e
 
 # Nama Image
-DOCKER_USERNAME="ilmi35" #Ganti dengan nama Docker Hub
+DOCKER_USERNAME="ilmi35" # Ganti dengan username Docker Hub
 IMAGE_NAME="$DOCKER_USERNAME/karsajobs-ui:latest"
 
-echo "🌀 Membuild Docker image"
-docker build -t $IMAGE_NAME .
+echo "🌀 Membuild Docker image : $IMAGE_NAME"
+docker build -t "$IMAGE_NAME" .
 
-echo "🌀 Login docker hub"
-echo $PASSWORD_DOCKER_HUB | docker login -u $DOCKER_USERNAME --password-stdin
+echo "🌀 Login ke Docker Hub"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-echo "🌀 Push Docker image ke Dokcer Hub"
-docker push $IMAGE_NAME
+echo "🌀 Push Docker image ke Docker Hub"
+docker push "$IMAGE_NAME"
 
-echo "✅ Berhasil push ke Dokcer Hub:$IMAGE_NAME"
+echo "✅ Berhasil push ke Docker Hub: $IMAGE_NAME"
